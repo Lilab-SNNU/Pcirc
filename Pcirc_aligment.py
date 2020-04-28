8caa6e38a03d658e5c577cb5f4b78d72618ad1a9
Shuwei Yin <32833252+ChasingWind@users.noreply.github.com>
2020-04-29 01:33:46 +0800

    Add files via upload

--- Pcirc_aligment.py
+++ Pcirc_aligment.py
@@ -10,7 +10,7 @@ parser.add_argument("-g",
                     help='reference genome files are Fasta')
 parser.add_argument("-G",
                     "--gtf",
-                    help='reference gff or gtf files')
+                    help='reference gtf files')
 parser.add_argument("-p",
                     "--num_threads",
                     default=4)
@@ -23,24 +23,30 @@ parser.add_argument("RNA_seq",
 args = parser.parse_args()
 
 inputs = args.RNA_seq
-threads = args.num_threads
+threads = int(args.num_threads)
 output_dir = args.output_dir
 gtf_filename = args.gtf
 genome_filename = args.genome
 
 genome_path = os.path.realpath(genome_filename)
 genome_index = genome_path[:genome_path.rfind('/')] + '/genome_index'
+
+
+print('='*20 + 'Bowtie begin' + '='*20)
 os.system("bowtie2-build %s %s" %
           (genome_filename, genome_index))
+print('='*20 + 'Bowtie end' + '='*20)
 
+print('='*20 + 'Tophat begin' + '='*20)
 num_fq = len(inputs)
 if num_fq == 1:
-    os.system("tophat2 -p %d -o %s -G %s %s" %
-              (threads, output_dir, gtf_filename, inputs[0]))
+    os.system("tophat2 -p %d -o %s -G %s %s %s" %
+              (threads, output_dir, gtf_filename, genome_index, inputs[0]))
 
 elif num_fq == 2:
-    os.system("tophat2 -p %d -o %s -G %s %s %s" %
-              (threads, output_dir, gtf_filename, inputs[0], inputs[1]))
+    os.system("tophat2 -p %d -o %s -G %s %s %s %s" %
+              (threads, output_dir, gtf_filename, genome_index, inputs[0], inputs[1]))
+print('='*20 + 'Tophat end' + '='*20)
 
 os.system("samtools view %s/unmapped.bam | "
           "awk \'{OFS=\"\t\"; print \">\"$1\"\n\"$10}\' - > "
